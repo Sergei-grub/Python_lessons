2ae66e32ca13c447468a0d94930d768f7401122b
Sergei <sergeygrubber@gmail.com>
2023-05-19 22:16:01 +0300

    Задачи с 1 семинара Python

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 
+
+c = 0
+
+print(c)
+c = bool(c)
+print(c)
+print(type(c))
+
+
+""" print("Введите строку: ")
+a = input()
+b = input('Введите второе число: ')
+
+print(a, ' + ', b, ' = ', a + b) """
+
+
+""" 
 n = 1.66
 s = 'Hello World'
 print(5, 7 , 8, n, type(s), "\'\"")
+
+# print(5)
+print()
+print(f"{n} - {s}") 
+"""
