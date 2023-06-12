b3a62e311fe88f2f4a7e2a02d32b26edb32192a5
Sergei <sergeygrubber@gmail.com>
2023-06-13 00:27:11 +0300

    Решение задач 34 и 36

--- Task_34-36.py
+++ Task_34-36.py
@@ -18,7 +18,7 @@
     **Вывод:** Парам пам-пам   """
 
 
-def help(rifma):
+""" def help(rifma):
     res = rifma.split(" ")
     lst_1 = []
     vowel = 'ауоыиэяюёе'
@@ -35,7 +35,7 @@ def help(rifma):
 
 
 rifma = 'пара-ра-рам рам-пам-папам па-ра-па-да'
-print(help(rifma))
+print(help(rifma)) """
 
 
 # ///////////////////////////////////////////////////////////////
@@ -53,9 +53,18 @@ print(help(rifma))
 **Ввод:** `print_operation_table(lambda x, y: x * y) ` 
 **Вывод:**
 1 2 3 4 5 6
-
 2 4 6 8 10 12
 3 6 9 12 15 18
 4 8 12 16 20 24
 5 10 15 20 25 30
 6 12 18 24 30 36 """
+
+
+def print_operation_table(operation, num_rows, num_columns):
+    for i in range(1, num_rows + 1):
+        print()
+        for j in range(1, num_columns + 1):
+            print(operation(i,j), end='\t')
+
+num_rows, num_columns = 6, 6
+print_operation_table(lambda i, j: i*j, num_rows, num_columns)
